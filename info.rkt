55d202dd67aaae8d97a4813e1202be1c29ace457
Jay McCarthy <jay.mccarthy@gmail.com>
2015-10-05 21:29:12 -0400

    deps

--- info.rkt
+++ info.rkt
@@ -1,7 +1,22 @@
 #lang info
 (define collection "racket-cheat")
-(define deps '())
-(define build-deps '())
+(define deps '("base"
+               "scribble-lib"
+               ))
+(define build-deps '("db-doc"
+                     "db-lib"
+                     "drracket"
+                     "net-doc"
+                     "net-lib"
+                     "parser-tools-doc"
+                     "parser-tools-lib"
+                     "pict-doc"
+                     "pict-lib"
+                     "racket-doc"
+                     "sandbox-lib"
+                     "slideshow-doc"
+                     "slideshow-lib"
+                     ))
 (define scribblings '(("racket-cheat.scrbl" () (getting-started))))
 (define pkg-desc "a cheat sheet for Racket")
 (define pkg-authors '(jay))
